60ae172a7e8a4da367cc2214a938c827aeda20f7
Eric Giovanola <ericgio@gmail.com>
2015-05-11 22:31:56 -0700

    Remove debugger from logout detector

--- app/webroot/js/utils/LogoutDetect.js
+++ app/webroot/js/utils/LogoutDetect.js
@@ -6,7 +6,6 @@
 define(['lib/Facebook/fb'], function() {
 
   FB.Event.subscribe('auth.logout', function(response) {
-    debugger;
     if (confirm('You have been logged out. Please log back in.')) {
       document.location = '/login';
     }
