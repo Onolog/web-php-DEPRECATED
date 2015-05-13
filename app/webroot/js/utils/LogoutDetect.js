1fd72b229cf549823785479b2202ae93845e7d91
Eric Giovanola <ericgio@gmail.com>
2015-05-12 23:03:12 -0700

    Periodically check for logout instead of listening to event

--- app/webroot/js/utils/LogoutDetect.js
+++ app/webroot/js/utils/LogoutDetect.js
@@ -5,10 +5,17 @@
  */
 define(['lib/Facebook/fb'], function() {
 
-  FB.Event.subscribe('auth.logout', function(response) {
-    if (confirm('You have been logged out. Please log back in.')) {
-      document.location = '/login';
+  var INTERVAL = 5 * 60 * 1000; // 5 mins
+
+  setInterval(function() {
+    FB.getLoginStatus(function(response) {
+      if (
+        response.status !== 'connected' &&
+        confirm('You have been logged out. Please log back in.')
+      ) {
+        document.location = '/login';
+      }
     }
-  });
+  }, INTERVAL);
 
 });
