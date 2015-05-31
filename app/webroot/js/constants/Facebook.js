19b8fff0e7b42b1a591a9a2d4c487ed75a56be68
Eric Giovanola <ericgio@gmail.com>
2015-05-31 16:10:39 -0700

    Remove ignored files

--- app/webroot/js/constants/Facebook.js
+++
@@ -1,9 +0,0 @@
-/**
- * Constants for Facebook API
- */
-define({
-  // Define the app id and app secret on individual machines both for security
-  // and because dev environments use a different version of the app.
-  APP_ID: '',
-  SECRET: ''
-});
