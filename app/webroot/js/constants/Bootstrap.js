0dc17e12a8d6dacb09671c9b3ad24cd6938a5aed
Eric Giovanola <ericgio@gmail.com>
2015-09-04 22:57:19 -0700

    Bootstrap Constants

---
+++ app/webroot/js/constants/Bootstrap.js
@@ -0,0 +1,21 @@
+/**
+ * Bootstrap
+ */
+define({
+
+  SIZE: {
+    LARGE: 'large',
+    DEFAULT: 'default',
+    SMALL: 'small',
+    XSMALL: 'xsmall'
+  },
+
+  USE: {
+    DEFAULT: 'default',
+    PRIMARY: 'primary',
+    SUCCESS: 'success',
+    INFO: 'info',
+    WARNING: 'warning',
+    DANGER: 'danger'
+  }
+});
