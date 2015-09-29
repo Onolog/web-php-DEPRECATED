e2f7f908bc1233e24f204f0e7bb3b86921f1a498
Eric Giovanola <ericgio@gmail.com>
2015-09-28 23:15:55 -0700

    Update/simplify login button

--- app/webroot/js/components/Facebook/FBLoginButton.react.js
+++ app/webroot/js/components/Facebook/FBLoginButton.react.js
@@ -1,6 +1,5 @@
 var React = require('react');
-
-var Button = require('../Button/Button.react');
+var Button = require('react-bootstrap/lib/Button');
 
 /**
  * FBLoginButton.react
@@ -11,28 +10,15 @@ var Button = require('../Button/Button.react');
 var FBLoginButton = React.createClass({
   displayName: 'FBLoginButton',
 
-  getInitialState: function() {
-    return {
-      signedOut: true
-    };    
-  },
-
   render: function() {
     return (
       <Button
         className="fbLogin"
-        customGlyph={<i className="fbIcon" />}
-        label={this._getLabel()}
         onClick={this.props.onClick}
-        size="large"
-        use="primary"
-      />
-    );
-  },
-
-  _getLabel: function() {
-    return (
-      this.state.signedOut ? 'Sign in with Facebook' : 'Sign out of Facebook'
+        bsSize="large"
+        bsStyle="primary">
+        <i className="fbIcon" /> Sign in with Facebook
+      </Button>
     );
   }
 });
