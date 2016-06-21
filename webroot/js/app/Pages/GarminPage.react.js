2ce8427515b6a4593e1d0c07d95f500c49a6af1b
Eric Giovanola <ericgio@gmail.com>
2016-06-21 02:09:29 -0700

    Update Garmin upload page

--- webroot/js/app/Pages/GarminPage.react.js
+++ webroot/js/app/Pages/GarminPage.react.js
@@ -1,36 +1,47 @@
-var moment = require('moment');
-var {Panel} = require('react-bootstrap');
-var React = require('react');
+import moment from 'moment';
+import {Panel} from 'react-bootstrap';
+import React from 'react';
 
-var Activity = require('app/Activities/Activity.react');
-var AppPage = require('components/Page/AppPage.react');
-var EmptyState = require('components/EmptyState.react');
-var FileInput = require('components/Forms/FileInput.react');
-var PageHeader = require('components/Page/PageHeader.react');
+import Activity from 'app/Activities/Activity.react';
+import AppPage from 'components/Page/AppPage.react';
+import EmptyState from 'components/EmptyState.react';
+import FileInput from 'components/Forms/FileInput.react';
+import PageHeader from 'components/Page/PageHeader.react';
 
-var {head} = require('lodash');
-var {metersToFeet, metersToMiles} = require('utils/distanceUtils');
-var FileParser = require('utils/parsers/FileParser');
-var GoogleTimezone = require('utils/GoogleTimezone');
+import {head} from 'lodash';
+import {metersToFeet, metersToMiles} from 'utils/distanceUtils';
+import FileParser from 'utils/parsers/FileParser';
+import GoogleTimezone from 'utils/GoogleTimezone';
 
 /**
  * GarminUploader.react
  */
-var GarminUploader = React.createClass({
+const GarminUploader = React.createClass({
   displayName: 'GarminUploader',
 
-  getInitialState: function() {
+  getInitialState() {
     return {
       activity: null,
     };
   },
 
-  render: function() {
+  render() {
     const {activity} = this.state;
+    const athlete = {
+      id: 517820043,
+      name: 'Eric Giovanola',
+    };
+    const shoes = {
+      id: 41,
+      name: 'ASICS DS Trainer 19.2',
+    };
+
     let contents = activity ?
       <Activity
         activity={this._normalizeActivity(activity)}
+        athlete={athlete}
         fill
+        shoes={shoes}
       /> :
       <EmptyState>
         No activity to display. Please upload a file.
@@ -40,7 +51,7 @@ var GarminUploader = React.createClass({
       <AppPage narrow>
         <PageHeader title="" />
         <Panel header={<h3>Choose a .tcx file</h3>}>
-          <FileInput onChange={this._onChange} />
+          <FileInput onChange={this._handleChange} />
         </Panel>
         <Panel>
           {contents}
@@ -52,14 +63,14 @@ var GarminUploader = React.createClass({
   /**
    * Convert a Garmin activity to the standardized format.
    */
-  _normalizeActivity: function(activity) {
-    var friends = [
+  _normalizeActivity(activity) {
+    const friends = [
       4280,
       700963,
       509191417,
     ].join(',');
 
-    var notes =
+    const notes =
       'Long run in Wunderlich Park with Sonderby, Turner, and Laney. ' +
       'Felt pretty good, probably pushed a bit too hard. HR was over ' +
       '153 most of the time. Felt a little beat up by the end, but ' +
@@ -67,50 +78,39 @@ var GarminUploader = React.createClass({
       '\n\n' +
       'http://connect.garmin.com/modern/activity/719673604';
 
-    var m = moment(activity.start_date);
+    return {
+      ...activity,
 
-    return Object.assign({}, activity, {
       // General activity data
       id: 0,
-      date: m.unix(),
-      start_date: m.format(),
+      start_date: moment(activity.start_date).format(),
 
       // Athlete-specific data
       distance: metersToMiles(activity.distance),
-      time: activity.duration,
+      // time: activity.duration,
       elevation_gain: metersToFeet(activity.elevation_gain),
       elevation_loss: metersToFeet(activity.elevation_loss),
-      friends: friends,
-      notes: notes,
-
-      // From foreign keys
-      athlete: {
-        id: 517820043,
-        name: 'Eric Giovanola',
-      },
-      shoes: {
-        id: 41,
-        name: 'ASICS DS Trainer 19.2',
-      },
-    });
+      friends,
+      notes,
+    };
   },
 
-  _onChange: function(evt) {
-    var files = evt.target.files;
-    var reader = new FileReader();
+  _handleChange(e) {
+    const {files} = e.target;
+    const reader = new FileReader();
 
     reader.onloadend = this._onLoadEnd;
     reader.readAsText(files[0]);
   },
 
-  _onLoadEnd: function(evt) {
-    if (evt.target.readyState === FileReader.DONE) {
-      var parser = new FileParser();
-      var activities = parser.parse(evt.target.result);
+  _onLoadEnd(e) {
+    if (e.target.readyState === FileReader.DONE) {
+      const parser = new FileParser();
+      const activities = parser.parse(e.target.result);
 
       // We currently only upload one file at a time
-      var activity = head(activities);
-      var start = activity.tracks[0][0];
+      const activity = head(activities);
+      const start = activity.tracks[0][0];
 
       // Get the timezone from the activity's geodata.
       GoogleTimezone({
