6ff8fda7ea9580c565f4dfb0eb0cc19c194b1c69
Eric Giovanola <ericgio@gmail.com>
2016-10-17 03:59:43 -0700

    Clarify build steps

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Remove existing built files.
+echo 'Cleaning up old files...'
 rm ./webroot/build/*.js
 
 # Rebuild files according to environment.
-echo 'Building files...'
+echo 'Building assets...'
 
 if [[ "$NODE_ENV" == "production" ]]; then
   ./node_modules/.bin/webpack --progress --colors --config webpack.prod.config.js
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Pull down new changes from remote master.
+echo 'Getting latest master from Github...'
 git pull origin master
 
 # Remove node modules and re-install.
+echo 'Reinstalling Node modules...'
 rm -rf ./node_modules
 npm install
 
