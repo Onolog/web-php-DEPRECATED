d6caf972508956be0403d325102233fa55ca4b4c
Eric Giovanola <ericgio@gmail.com>
2017-02-14 12:09:56 -0800

    Add `AlertModal` component

---
+++ webroot/js/components/Modals/AlertModal.react.js
@@ -0,0 +1,33 @@
+import React, {PropTypes} from 'react';
+import {Alert, Modal} from 'react-bootstrap';
+
+import './css/AlertModal.css';
+
+const AUTOHIDE_INTERVAL = 10 * 1000; // 10 seconds
+
+const AlertModal = ({autohideInterval, bsStyle, children, onHide, show}) => (
+  <Modal
+    backdrop={false}
+    bsSize="small"
+    dialogClassName="alert-modal"
+    onEntered={() => setInterval(onHide, autohideInterval)}
+    onHide={onHide}
+    show={show}>
+    <Alert bsStyle={bsStyle} onDismiss={onHide}>
+      {children}
+    </Alert>
+  </Modal>
+);
+
+AlertModal.propTypes = {
+  autohideInterval: PropTypes.number,
+  onHide: PropTypes.func.isRequired,
+  show: PropTypes.bool,
+};
+
+AlertModal.defaultProps = {
+  autohideInterval: AUTOHIDE_INTERVAL,
+  show: false,
+};
+
+export default AlertModal;
