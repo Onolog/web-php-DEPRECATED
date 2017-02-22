76b678495c9f90c26c713652ea723c4ac0f24352
Eric Giovanola <ericgio@gmail.com>
2017-02-21 21:49:59 -0800

    Improve client-side 404 page

--- webroot/js/controllers/NotFoundController.react.js
+++ webroot/js/controllers/NotFoundController.react.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {browserHistory} from 'react-router';
 
-import AppPage from 'components/Page/AppPage.react';
-import PageHeader from 'components/Page/PageHeader.react';
+import BaseAppPage from 'components/Page/BaseAppPage.react';
+import Link from 'components/Link/Link.react';
+import MaterialIcon from 'components/Icons/MaterialIcon.react';
+import Middot from 'components/Middot.react';
+
+import './css/NotFound.scss';
 
 /**
  * NotFoundController.react
@@ -9,12 +14,33 @@ import PageHeader from 'components/Page/PageHeader.react';
  * Catch-all page if a route doesn't match.
  */
 const NotFoundController = props => (
-  <AppPage className="error-page">
-    <PageHeader title="Sorry, this page isn't available" />
-    <p>
-      The link you followed may be broken, or the page may have been removed.
-    </p>
-  </AppPage>
+  <BaseAppPage className="error-page">
+    <div className="container">
+      <h2>Page not found.</h2>
+      <MaterialIcon
+        className="error-page-icon"
+        icon="alert-octagram"
+      />
+      <p>
+        The link you followed may be broken, or the page may have been removed.
+      </p>
+      <ul className="list-inline">
+        <li>
+          <Link onClick={browserHistory.goBack}>
+            Back
+          </Link>
+        </li>
+        <li>
+          <Middot />
+        </li>
+        <li>
+          <Link href="/">
+            Home
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </BaseAppPage>
 );
 
 module.exports = NotFoundController;
