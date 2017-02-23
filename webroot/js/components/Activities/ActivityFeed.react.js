180c2ac5ed947c192da505adcecc8c7e718bcdfc
Eric Giovanola <ericgio@gmail.com>
2017-02-23 11:18:22 -0800

    Basic `ActivityFeed` component for profile

---
+++ webroot/js/components/Activities/ActivityFeed.react.js
@@ -0,0 +1,48 @@
+import cx from 'classnames';
+import moment from 'moment-timezone';
+import React, {PropTypes} from 'react';
+
+import EmptyState from 'components/EmptyState.react';
+
+import './css/ActivityFeed.scss';
+
+const DATE_FORMAT = 'dddd, MMMM Do, YYYY';
+
+const ActivityFeedItem = ({activity, className}) => {
+  const {distance, duration, start_date, timezone} = activity;
+
+  return (
+    <div className={cx('activity-feed-item')}>
+      <div>{moment.tz(start_date, timezone).format(DATE_FORMAT)}</div>
+      <div>{distance}</div>
+      <div>{duration}</div>
+    </div>
+  );
+};
+
+const ActivityFeed = ({activities, className}) => {
+  if (!activities.length) {
+    return (
+      <EmptyState>
+        No recent activities.
+      </EmptyState>
+    );
+  }
+
+  return (
+    <div className={cx('activity-feed', className)}>
+      {activities.map(a => (
+        <ActivityFeedItem
+          activity={a}
+          key={a.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+ActivityFeed.propTypes = {
+  activities: PropTypes.array.isRequired,
+};
+
+export default ActivityFeed;
