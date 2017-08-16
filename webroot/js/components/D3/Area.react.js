d7d1e7914408c53c95b0d0feee8359e8eeff7cc6
Eric Giovanola <ericgio@gmail.com>
2017-08-15 23:40:15 -0700

    Lint errors

--- webroot/js/components/D3/Area.react.js
+++ webroot/js/components/D3/Area.react.js
@@ -3,23 +3,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {findDOMNode} from 'react-dom';
 
+import {getInnerHeight} from 'utils/d3Utils';
+
 import './css/d3-area.css';
-
-const MARGIN = {
-  bottom: 30,
-  left: 40,
-  right: 30,
-  top: 20,
-};
-
-const getInnerHeight = height => height - MARGIN.top - MARGIN.bottom;
-const getInnerWidth = width => width - MARGIN.left - MARGIN.right;
 
 class Area extends React.Component {
   static propTypes = {
     data: PropTypes.array.isRequired,
     height: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired,
     xScale: PropTypes.func.isRequired,
     yScale: PropTypes.func.isRequired,
   }
@@ -37,7 +28,7 @@ class Area extends React.Component {
   }
 
   _drawArea = () => {
-    const {data, height, width, xScale, yScale} = this.props;
+    const {data, height, xScale, yScale} = this.props;
 
     const area = d3.area()
       .x(d => xScale(d.xVal))
